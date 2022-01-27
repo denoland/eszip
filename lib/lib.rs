d259282878dbfa639a089c6e1f9ea9f72646cb61
Divy Srivastava <dj.srivastava23@gmail.com>
2022-01-27 21:39:20 +0530

    stuff

--- lib/lib.rs
+++ lib/lib.rs
@@ -1,23 +1,25 @@
-use futures::io::AsyncRead;
 use futures::io::Error;
 use futures::task::Context;
 use futures::task::Poll;
-use js_sys::ArrayBuffer;
+use futures::AsyncRead;
+use futures::FutureExt;
 use js_sys::Uint8Array;
-use wasm_bindgen::prelude::*;
-use web_sys::ReadableStreamBYOBReader;
 use std::pin::Pin;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+
+#[cfg(web_sys_unstable_apis)]
+use web_sys::ReadableStreamByobReader;
 
 struct Stream {
-  inner: ReadableStreamBYOBReader,
+  inner: ReadableStreamByobReader,
+  fut: Option<JsFuture>,
 }
 
 impl Stream {
-  fn new(inner: ReadableStreamBYOBReader) -> Self {
-    Self {
-      inner,
-      buffer: Uint8Array::new(),
-    }
+  fn new(inner: ReadableStreamByobReader) -> Self {
+    Self { inner, fut: None }
   }
 }
 
@@ -39,9 +41,9 @@ impl AsyncRead for Stream {
     let fut = match self.fut.as_mut() {
       Some(fut) => fut,
       None => {
-        let buffer = Uint8Array::new_with_length(buf.len())
-          .unchecked_into::<ArrayBufferView>();
-        let fut = JsFuture::from(self.inner.read_with_array_buffer_view(&buffer));
+        let buffer = Uint8Array::new_with_length(buf.len() as u32);
+        let fut =
+          JsFuture::from(self.inner.read_with_array_buffer_view(&buffer));
         self.fut.insert(fut)
       }
     };
@@ -49,19 +51,19 @@ impl AsyncRead for Stream {
     let result = futures::ready!(fut.poll_unpin(cx));
     self.fut = None;
 
-    match js_ready {
+    match result {
       Ok(result) => {
         let result = result.unchecked_into::<ReadResult>();
         match result.is_done() {
-         true => Poll::Ready(Ok(0)),
-         false => {
-           let value = result.value().unwrap_throw();
-           let length = value.byte_length() as usize;
-           value.copy_to(&mut buf[0..length]);
-           Poll::Ready(Ok(length))
-         }
+          true => Poll::Ready(Ok(0)),
+          false => {
+            let value = result.value().unwrap_throw();
+            let length = value.byte_length() as usize;
+            value.copy_to(&mut buf[0..length]);
+            Poll::Ready(Ok(length))
+          }
         }
-      },
+      }
       // TODO(@littledivy): handle error
       Err(_) => todo!(),
     }
@@ -69,4 +71,7 @@ impl AsyncRead for Stream {
 }
 
 #[wasm_bindgen]
-pub async fn eszip_parse(stream: ReadableStreamBYOBReader) {}
+pub async fn eszip_parse(reader: ReadableStreamByobReader) {
+  let stream = Stream::new(reader);
+  let eszip = eszip::EszipV2::parse(stream);
+}
