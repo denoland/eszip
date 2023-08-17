935547c8486b463e8b170aee1822ae4276fd4e28
David Sherret <dsherret@gmail.com>
2023-08-17 12:19:33 -0400

    Probably fix.

--- build_npm.ts
+++ build_npm.ts
@@ -1,4 +1,4 @@
-import { build, emptyDir } from "https://deno.land/x/dnt@0.37.0/mod.ts";
+import { build, emptyDir } from "https://deno.land/x/dnt@0.38.1/mod.ts";
 
 await emptyDir("./npm");
 Deno.mkdirSync("npm/esm", { recursive: true });
