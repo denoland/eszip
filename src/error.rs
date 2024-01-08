412a96479934539fc070a15efd9a9cd3e3ca3a05
David Sherret <dsherret@gmail.com>
2024-01-08 14:07:36 -0500

    chore: bump copyright year to 2024

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
 use thiserror::Error;
 
 #[derive(Debug, Error)]
--- src/examples/viewer.rs
+++ src/examples/viewer.rs
@@ -1,3 +1,5 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
 use futures::io::AllowStdIo;
 use futures::io::BufReader;
 
