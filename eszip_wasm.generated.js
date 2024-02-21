4ac3b5c3c04d5279a6808a5c00249de39e161cc2
denobot <denobot@users.noreply.github.com>
2024-02-21 21:02:46 +0000

    Publish 6d24bb4295d8ccfa0e75eb92a2d7afdcf46cb5f3

--- eszip_wasm.generated.js
+++ eszip_wasm.generated.js
@@ -2,7 +2,7 @@
 // @ts-nocheck: generated
 // deno-lint-ignore-file
 // deno-fmt-ignore-file
-// source-hash: 8b21838a77e620ba78e39affbbff1ed880ad4fa2
+// source-hash: f4bd6400a8b77e4fd97f2805e25f5adfd8fbafcf
 let wasm;
 
 const heap = new Array(128).fill(undefined);
@@ -413,9 +413,17 @@ const imports = {
       const ret = BigInt.asUintN(64, arg0);
       return addHeapObject(ret);
     },
-    __wbg_new_d331494ab60a8491: function (arg0, arg1) {
-      const ret = new TypeError(getStringFromWasm0(arg0, arg1));
+    __wbg_new_34c624469fb1d4fd: function () {
+      const ret = new Array();
       return addHeapObject(ret);
+    },
+    __wbindgen_string_new: function (arg0, arg1) {
+      const ret = getStringFromWasm0(arg0, arg1);
+      return addHeapObject(ret);
+    },
+    __wbg_push_906164999551d793: function (arg0, arg1) {
+      const ret = getObject(arg0).push(getObject(arg1));
+      return ret;
     },
     __wbg_length_d99b680fd68bf71b: function (arg0) {
       const ret = getObject(arg0).length;
@@ -445,17 +453,9 @@ const imports = {
       const ret = new Uint8Array(getObject(arg0));
       return addHeapObject(ret);
     },
-    __wbg_new_34c624469fb1d4fd: function () {
-      const ret = new Array();
+    __wbg_new_d331494ab60a8491: function (arg0, arg1) {
+      const ret = new TypeError(getStringFromWasm0(arg0, arg1));
       return addHeapObject(ret);
-    },
-    __wbindgen_string_new: function (arg0, arg1) {
-      const ret = getStringFromWasm0(arg0, arg1);
-      return addHeapObject(ret);
-    },
-    __wbg_push_906164999551d793: function (arg0, arg1) {
-      const ret = getObject(arg0).push(getObject(arg1));
-      return ret;
     },
     __wbg_String_88810dfeb4021902: function (arg0, arg1) {
       const ret = String(getObject(arg1));
@@ -783,8 +783,8 @@ const imports = {
       const ret = getObject(arg0).queueMicrotask;
       return addHeapObject(ret);
     },
-    __wbindgen_closure_wrapper9776: function (arg0, arg1, arg2) {
-      const ret = makeMutClosure(arg0, arg1, 261, __wbg_adapter_46);
+    __wbindgen_closure_wrapper9852: function (arg0, arg1, arg2) {
+      const ret = makeMutClosure(arg0, arg1, 262, __wbg_adapter_46);
       return addHeapObject(ret);
     },
   },
