6886c2af18f89211ea3f057c26db8d4506a9407f
David Sherret <dsherret@gmail.com>
2025-05-27 16:56:03 -0400

    this is fine

--- lib/lib.rs
+++ lib/lib.rs
@@ -327,16 +327,8 @@ pub async fn build_eszip(
       BuildOptions {
         resolver: Some(&resolver),
         module_analyzer: &analyzer,
-        module_info_cacher: Default::default(),
-        is_dynamic: false,
-        skip_dynamic_deps: false,
-        passthrough_jsr_specifiers: false,
-        executor: Default::default(),
         file_system: &sys_traits::impls::RealSys,
-        jsr_url_provider: Default::default(),
-        locker: None,
-        npm_resolver: None,
-        reporter: None,
+        ..Default::default()
       },
     )
     .await;
