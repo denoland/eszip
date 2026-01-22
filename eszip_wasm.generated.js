e9c6a3ac90254501504fbbc513a0adc808e84cba
denobot <denobot@users.noreply.github.com>
2026-01-22 19:10:21 +0000

    Publish 7d727cd7b02747f4bbcbd8e07ce387e01e2eafaa

--- eszip_wasm.generated.js
+++ eszip_wasm.generated.js
@@ -431,10 +431,6 @@ const imports = {
       const ret = BigInt.asUintN(64, arg0);
       return addHeapObject(ret);
     },
-    __wbg_new_5dd86ebc917d9f52: function (arg0, arg1) {
-      const ret = new TypeError(getStringFromWasm0(arg0, arg1));
-      return addHeapObject(ret);
-    },
     __wbg_new_16b304a2cfa7ff4a: function () {
       const ret = new Array();
       return addHeapObject(ret);
@@ -446,6 +442,10 @@ const imports = {
     __wbg_push_a5b05aedc7234f9f: function (arg0, arg1) {
       const ret = getObject(arg0).push(getObject(arg1));
       return ret;
+    },
+    __wbg_new_5dd86ebc917d9f52: function (arg0, arg1) {
+      const ret = new TypeError(getStringFromWasm0(arg0, arg1));
+      return addHeapObject(ret);
     },
     __wbg_new_28c511d9baebfa89: function (arg0, arg1) {
       const ret = new Error(getStringFromWasm0(arg0, arg1));
@@ -791,7 +791,7 @@ const imports = {
       const ret = getObject(arg0).queueMicrotask;
       return addHeapObject(ret);
     },
-    __wbindgen_closure_wrapper10909: function (arg0, arg1, arg2) {
+    __wbindgen_closure_wrapper10906: function (arg0, arg1, arg2) {
       const ret = makeMutClosure(arg0, arg1, 862, __wbg_adapter_46);
       return addHeapObject(ret);
     },
